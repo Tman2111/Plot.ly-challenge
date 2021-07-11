c2528f6028633c1ac62eac3918ce28903dc110de
Tman <76560394+Tman2111@users.noreply.github.com>
2021-07-11 11:48:04 -0400

    horizontal bar

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -31,4 +31,26 @@ function init (){
 };
 init ();   
 
-     
+  // Horizontal Bar chart 
+  function barChart(subjectId){
+    d3.json('samples.json').then((data)=>{
+        var samples = data.samples;
+        var ID = samples.map(row=>row.id).indexOf(subjectId);
+        var otuValueTen = samples.map(row=>row.sample_values);
+        var otuValueTen = otuValueTen[ID].slice(0,10).reverse();
+        var otuIdTen = samples.map(row=>row.otu_ids);
+        var otuIdTen = otuIdTen[ID].slice(0,10);
+        var otuLabelTen = samples.map(row=>row.otu_labels).slice(0,10);
+        
+        var trace={
+            x: otuValueTen,
+            y: otuIdTen.map(r=>`UTO ${r}`),
+            text: otuLabelTen,
+            type:'bar',
+            orientation:'h'
+        }
+        var layout = {title: "Top 10 OTUs Found", margin: { t: 30, l: 150 }};
+  
+       Plotly.newPlot('bar',[trace],layout);
+    })
+};   
